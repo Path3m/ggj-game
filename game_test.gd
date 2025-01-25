446962ac414268deafbc6fc313a5781d3481db23
Aurel Hamon <hamon.aurel@orange.fr>
2025-01-25 11:04:21 +0100

    sorting file in folder

--- game_test.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var enemy = $colleague_enemy
-@onready var char = $false_player
-
-func _process(delta: float) -> void:
-	print("Enenmy: ", enemy.position, "| Char :", char.position);
-	pass
