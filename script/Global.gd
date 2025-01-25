7dbc0e2200432ab2a135573786cb8d1d05513e58
Aurel Hamon <hamon.aurel@orange.fr>
2025-01-25 17:51:12 +0100

    add fucntionnality of dialogue launching on enemy contact

--- script/Global.gd
+++ script/Global.gd
@@ -1,8 +1,16 @@
 extends Node
 
+const dialogue_list : Array[String] = [
+	"res://dialogue/complotiste.dialogue"
+];
+
 var in_bubble_world : bool = false;
 
 func switch_world() -> void:
 	in_bubble_world = !in_bubble_world;
+	
+func select_random_dialogue() -> String:
+	var i : int = randi() % dialogue_list.size();
+	return dialogue_list[i];
 
 signal changed_world;
