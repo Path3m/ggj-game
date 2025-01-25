2df7de28387a2d3dca140e3a1e38d184a09f266d
tristan <lallemand.t@me.com>
2025-01-25 19:54:02 +0100

    .

--- script/perso.gd
+++ script/perso.gd
@@ -21,17 +21,18 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	#sprite deplacement
-	if Input.is_action_pressed("ui_right"):
-		$Sprite2D.flip_h = false
-		#$Sprite2D.play("NomTODO")
-	elif Input.is_action_pressed("ui_left"):
-		$Sprite2D.flip_h = true
-		#$Sprite2D.play("NomTODO")
-	#if Input.is_action_pressed("ui_up"):
-		#$Sprite2D.play("NomTODO")
-	#if Input.is_action_pressed("ui_down"):
-		#$Sprite2D.play("NomTODO")
-	
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed ("ui_left") or Input.is_action_pressed ("ui_up") or Input.is_action_pressed ("ui_down"):
+		if Input.is_action_pressed("ui_right"):
+			$MC_mouv.play("MC_droite")
+		if Input.is_action_pressed("ui_left"):
+			$MC_mouv.play("MC_gauche")
+		if Input.is_action_pressed("ui_up"):
+			$MC_mouv.play("MC_haut")
+		if Input.is_action_pressed("ui_down"):
+			$MC_mouv.play("MC_bas")
+	else:
+		$MC_mouv.play("MC_bas")
+		
 	if Input.is_action_just_pressed("change_world"):
 		Global.switch_world();
 		Global.changed_world.emit();
