94458c3871747d5dd795d56612f129f699b76c29
kat-eul <katel.hgd@gmail.com>
2025-01-26 13:46:54 +0100

    feat : animation de debut sans dialogue

--- script/menu_principal.gd
+++ script/menu_principal.gd
@@ -2,8 +2,15 @@ extends Control
 
 @onready var first_level = preload("res://scene/level1.tscn")
 
+func _ready() -> void:
+	$openingAnim.hide()
+	$bg_animation.hide()
+
 func _on_start_pressed():
-	await get_tree().create_timer(1)
+	$openingAnim.show()
+	$bg_animation.show()
+	$openingAnim.play("opening")
+	await get_tree().create_timer(12).timeout
 	get_tree().change_scene_to_packed(first_level)
 
 func _on_quit_pressed():
